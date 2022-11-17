314cbda927cc360ab45d4b61616d4605a47030d7
Riswan Agam <riswanagam@gmail.com>
2022-11-18 01:35:17 +0700

    #add eureka client

--- src/main/java/com/ra/bioskop/StudioApplication.java
+++ src/main/java/com/ra/bioskop/StudioApplication.java
@@ -2,8 +2,10 @@ package com.ra.bioskop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudioApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ra/bioskop/controller/ScheduleController.java
+++ src/main/java/com/ra/bioskop/controller/ScheduleController.java
@@ -1,5 +1,6 @@
 package com.ra.bioskop.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ra.bioskop.dto.model.FilmDTO;
 import com.ra.bioskop.dto.request.ScheduleRequest;
 import com.ra.bioskop.dto.response.Response;
@@ -13,9 +14,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 import static com.ra.bioskop.exception.BioskopException.throwException;
@@ -37,29 +43,32 @@ public class ScheduleController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> addSchedule(@RequestBody ScheduleRequest scheduleRequest, HttpServletRequest request)  {
+    public ResponseEntity<?> addSchedule(@RequestBody ScheduleRequest scheduleRequest,
+                                         HttpServletRequest request, HttpServletResponse response)  {
         try {
-            return webClient
+            var res = webClient
                     .get()
                     .uri(baseFilmClient + "/api/v1/films/detail?id=" + scheduleRequest.getFilmId())
                     .header(Constants.HEADER, Constants.TOKEN_PREFIX + request.getAttribute("validToken"))
-                    .retrieve().bodyToMono(Response.class).map(res -> {
+                    .retrieve().bodyToMono(Response.class)
+                    .onErrorComplete()
+                    .block();
 
-                        LinkedHashMap<String, Object> linkedHashMap = (LinkedHashMap<String, Object>) res.getData();
+            if(res == null)
+                throw throwException(ExceptionType.NOT_FOUND, HttpStatus.NOT_FOUND, Constants.NOT_FOUND_MSG);
 
-                        if(linkedHashMap == null)
-                            throw throwException(ExceptionType.NOT_FOUND, HttpStatus.NOT_FOUND, Constants.NOT_FOUND_MSG);
+            LinkedHashMap<String, Object> linkedHashMap = (LinkedHashMap<String, Object>) res.getData();
 
-                        FilmDTO filmDTO = new FilmDTO();
-                        filmDTO.setFilmCode((String) linkedHashMap.get("filmCode"));
-                        filmDTO.setTitle((String) linkedHashMap.get("title"));
-                        filmDTO.setOnShow((Boolean) linkedHashMap.get("onShow"));
-                        filmDTO.setOverview((String) linkedHashMap.get("overview"));
+            FilmDTO filmDTO = new FilmDTO();
+            filmDTO.setFilmCode((String) linkedHashMap.get("filmCode"));
+            filmDTO.setTitle((String) linkedHashMap.get("title"));
+            filmDTO.setOnShow((Boolean) linkedHashMap.get("onShow"));
+            filmDTO.setOverview((String) linkedHashMap.get("overview"));
 
-                        scheduleService.addSchedule(scheduleRequest, filmDTO);
-                        return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
-                                Constants.SUCCESS_MSG, null));
-                    }).block();
+            scheduleService.addSchedule(scheduleRequest, filmDTO);
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
+                    Constants.SUCCESS_MSG, null));
+
         } catch (BioskopException.EntityNotFoundException e) {
             return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(), new Date(), e.getMessage()),
                     e.getStatusCode());
